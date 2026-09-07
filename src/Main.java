67aabe5a101dda2fd416e7bebcf02143599fd9bf
Drtikolnik <drtluk00@gasos-ro.cz>
2026-09-07 10:17:27 +0200

    d

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
+        Student s1 = new Student("Karel Havlíček", 3, 2.5);
+        Student s2 = new Student("Karel Čapek", 2, 1.3);
+        Student s3 = new Student("Josef Čapek", 2, 3.8);
+
+
+
+
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,92 @@
 public class Student {
     private String jmeno;
+    private int rocnik;
+    private double prumer;
 
+
+    public Student(String jmeno, int rocnik, double prumer) {
+        this.jmeno = jmeno;
+        this.rocnik = rocnik;
+        this.prumer = prumer;
+    }
+
+
+
+    public String toString(){
+        return jmeno + "ročík " + rocnik + ", průměr " +prumer;
+    }
+
+    public boolean equals(Student jedna, Student dva) {
+        if(jedna.jmeno.equals(dva.jmeno) && jedna.prumer==dva.prumer){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+    public void zlepsiPrmer(double oKolik){
+        if((prumer-oKolik)>=1){
+            prumer=prumer-oKolik;
+        }else{
+            System.err.println("ZADANÁ ŠPATNÁ HODNOTA");
+        }
+    }
+
+    public void klasifikace(){
+        if(prumer<2){
+            System.out.println("VÝBORNÝ");
+        }
+        if(prumer>=2 && prumer<3){
+            System.out.println("CHVALITEBNÝ");
+        }
+        if(prumer>=3 && prumer<4){
+            System.out.println("DOBRÝ");
+        }
+        if(prumer>=4 && prumer<5){
+            System.out.println("DOSTATEČNÝ");
+        }
+        if(prumer>=5){
+            System.out.println("NEDOSTATEČNÝ");
+        }
+    }
+
+
+
+
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public void setJmeno(String jmeno) {
+        this.jmeno = jmeno;
+    }
+
+    public int getRocnik() {
+        return rocnik;
+    }
+
+    public void setRocnik(int rocnik) {
+        if (rocnik==1 || rocnik==2 || rocnik==3 || rocnik==4) {
+            this.rocnik = rocnik;
+        }else{
+            System.err.println("CHYBNÁ HODNOTA ROČNÍKU");
+        }
+    }
+
+    public double getPrumer() {
+        return prumer;
+
+
+    }
+
+    public void setPrumer(double prumer) {
+        if(prumer>=1 && prumer<=5){
+            this.prumer = prumer;
+        }else{
+            System.err.println("CHYBNÁ HODNOTA PRŮMĚRU");
+        }
+
+    }
 }
