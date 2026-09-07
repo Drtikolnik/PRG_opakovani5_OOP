0d894ecec307b8e4876fa37c7af72980590c16b5
Drtikolnik <drtluk00@gasos-ro.cz>
2026-09-07 10:21:45 +0200

    d

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,20 @@ public class Main {
 
         Student s1 = new Student("Karel Havlíček", 3, 2.5);
         Student s2 = new Student("Karel Čapek", 2, 1.3);
-        Student s3 = new Student("Josef Čapek", 2, 3.8);
+        Student s3 = new Student("Karel Čapek", 2, 1.3);
 
+
+        if(s2.equals(s2, s3)){
+            System.out.println("ROVNAJÍ SE");
+        }else{
+            System.out.println("NEROVNAJÍ SE");
+        }
+
+        s1.klasifikace();
+        s1.zlepsiPrmer(0.5);
+        s1.zlepsiPrmer(0.5);
+        System.out.println(s1.getPrumer());
+        s1.klasifikace();
 
 
 
